63acb7f2000c7fa19eee29f1f5ead5c1c49ae7c5
unknown <Administrator@SSSLBG4NB015.SONATA.LOCAL>
2021-07-22 18:33:03 +0530

    File Upload

---
+++ Angular10UploadFile/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UploadFilesComponent } from './components/upload-files/upload-files.component';
+import { HttpClientModule } from '@angular/common/http';
+@NgModule({
+  declarations: [
+    AppComponent,
+    UploadFilesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
